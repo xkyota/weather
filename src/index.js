060ec7aab1f5c1a3fd8d9ae5dc9235120d9b4c44
Maksym Durikhin <max1mkasss.mail@gmail.com>
2026-01-06 20:53:12 +0200

    sections

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+
+import Chart from './components/Chart';
+import EightDayForecast from './components/Days-forecast';
+import DetailedWeatherCards from './components/Detailed-weather-cards';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import NaturalSlider from './components/Natural-slider.jsx';
+import PetsNews from './components/Pets-news';
+import WeatherCards from './components/Weather-cards';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Hero />
+    <WeatherCards />
+    <DetailedWeatherCards />
+    <Chart />
+    <EightDayForecast />
+    <PetsNews />
+    <NaturalSlider />
+    <Footer />
   </React.StrictMode>
 );
