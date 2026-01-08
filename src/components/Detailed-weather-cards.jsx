d7ea3598dff440bff12bcd40da3a69de1480748d
supriseeeeef <romankorniiets@gmail.com>
2026-01-08 21:46:37 +0200

    addedImages

--- src/components/Detailed-weather-cards.jsx
+++ src/components/Detailed-weather-cards.jsx
@@ -1,4 +1,10 @@
-import '../styles/Detailed-weather-cards.css';
+import '../styles/Detailed-weather-cards.css'
+
+import FeelsLikeWarm from './img-Detailed-weather-cards/FeelsLike-warm.svg'
+import HumidityRain from './img-Detailed-weather-cards/Humidity-rain.svg'
+import PressureHight from './img-Detailed-weather-cards/Pressure-hight.svg'
+import WindSpeed from './img-Detailed-weather-cards/WindSpeed.svg'
+import VisibilityUnlimited from './img-Detailed-weather-cards/Visibility-unlimited.svg'
 
 const DetailedWeatherCards = () => {
     return (
@@ -7,7 +13,7 @@ const DetailedWeatherCards = () => {
                 <li className="detailed-cards__item">
                     <p className="detailed-cards__value">Feels like</p>
                     <p className="detailed-cards__label"></p>
-                    <img className="detailed-cards__icon" src="" alt="" />
+                    <img className="detailed-cards__icon" src={FeelsLikeWarm} alt="Feels like data" />
                 </li>
 
                 <li className="detailed-cards__item">
@@ -20,29 +26,29 @@ const DetailedWeatherCards = () => {
                 <li className="detailed-cards__item">
                     <p className="detailed-cards__value">Humidity</p>
                     <p className="detailed-cards__label"></p>
-                    <img className="detailed-cards__icon" src="" alt="" />
+                    <img className="detailed-cards__icon" src={HumidityRain} alt="Humidity data" />
                 </li>
 
                 <li className="detailed-cards__item">
                     <p className="detailed-cards__value">Pressure</p>
                     <p className="detailed-cards__label"></p>
-                    <img className="detailed-cards__icon" src="" alt="" />
+                    <img className="detailed-cards__icon" src={PressureHight} alt="Pressure data" />
                 </li>
 
                 <li className="detailed-cards__item">
                     <p className="detailed-cards__value">Wind speed</p>
                     <p className="detailed-cards__label"></p>
-                    <img className="detailed-cards__icon" src="" alt="" />
+                    <img className="detailed-cards__icon" src={WindSpeed} alt="Wind speed data" />
                 </li>
 
                 <li className="detailed-cards__item">
                     <p className="detailed-cards__value">Visibility</p>
                     <p className="detailed-cards__label"></p>
-                    <img className="detailed-cards__icon" src="" alt="" />
+                    <img className="detailed-cards__icon" src={VisibilityUnlimited} alt="Visibility data" />
                 </li>
             </ul>
         </div>
     )
 }
 
-export default DetailedWeatherCards 
+export default DetailedWeatherCards
