72d62706ae330096b581e552fdb721a007ff0fc2
supriseeeeef <romankorniiets@gmail.com>
2026-01-08 21:27:45 +0200

    done html + css Detailed-weather-cards

--- src/components/Detailed-weather-cards.jsx
+++ src/components/Detailed-weather-cards.jsx
@@ -1,7 +1,48 @@
 import '../styles/Detailed-weather-cards.css';
 
 const DetailedWeatherCards = () => {
-    return; 
-};
+    return (
+        <div className="detailed-cards">
+            <ul className="detailed-cards__list">
+                <li className="detailed-cards__item">
+                    <p className="detailed-cards__value">Feels like</p>
+                    <p className="detailed-cards__label"></p>
+                    <img className="detailed-cards__icon" src="" alt="" />
+                </li>
 
-export default DetailedWeatherCards; 
+                <li className="detailed-cards__item">
+                    <p className="detailed-cards__value">Min ℃</p>
+                    <p className="detailed-cards__label"></p>
+                    <p className="detailed-cards__value">Max ℃</p>
+                    <p className="detailed-cards__label"></p>
+                </li>
+
+                <li className="detailed-cards__item">
+                    <p className="detailed-cards__value">Humidity</p>
+                    <p className="detailed-cards__label"></p>
+                    <img className="detailed-cards__icon" src="" alt="" />
+                </li>
+
+                <li className="detailed-cards__item">
+                    <p className="detailed-cards__value">Pressure</p>
+                    <p className="detailed-cards__label"></p>
+                    <img className="detailed-cards__icon" src="" alt="" />
+                </li>
+
+                <li className="detailed-cards__item">
+                    <p className="detailed-cards__value">Wind speed</p>
+                    <p className="detailed-cards__label"></p>
+                    <img className="detailed-cards__icon" src="" alt="" />
+                </li>
+
+                <li className="detailed-cards__item">
+                    <p className="detailed-cards__value">Visibility</p>
+                    <p className="detailed-cards__label"></p>
+                    <img className="detailed-cards__icon" src="" alt="" />
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default DetailedWeatherCards 
