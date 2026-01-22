8feb4c99d4347be6118c2d34fa236f9d95a635c3
PvNastia <nastiapogorelova6@gmail.com>
2026-01-22 20:33:47 +0100

    made new API link and add mobile version in petsNews section

--- src/components/Pets-news.jsx
+++ src/components/Pets-news.jsx
@@ -3,66 +3,80 @@ import '../styles/Pets-news.css';
 import {
   useEffect,
   useState,
+  useRef
 } from 'react';
 
 import axios from 'axios';
 
+/* const getValidDate = () => {
+  const date = new Date();
+  date.setDate(date.getDate() - 7);
+  return date.toISOString().split('T')[0];
+}; */
 const PetsNews = () => {
-    const [images, setImages] = useState([]);
-    const [page, setPage] = useState(1);
-    const [loading, setLoading] = useState(false);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
+const isFetched = useRef(false);
+  const fetchImages = async (perPage =4) => {
+    setLoading(true);
+    try {
+    /*   const API_KEY = "54021536-dcf561e7ed11353144e71127c";
+      const fromDate = getValidDate();
+const url = `https://newsapi.org/v2/everything?q=pets&from=${fromDate}&sortBy=publishedAt&pageSize=${perPage}&page=${page}&apiKey=${API_KEY}`; */
+const url = `https://api.thecatapi.com/v1/images/search?limit=${perPage}&page=${page}&order=DESC`;
 
-    const fetchImages = async (perPage = 12) => {
-        setLoading(true);
-        try {
-            const API_KEY = "54021536-dcf561e7ed11353144e71127c";
-            const url = `https://api.pixabay.com/api/?key=${API_KEY}&q=pets+news&image_type=photo&orientation=horizontal&per_page=${perPage}&page=${page}`;
+      const response = await axios.get(url);
+
+ if (Array.isArray(response.data)) {
+        setImages(prev => [...prev, ...response.data]);
+        setPage(prevPage => prevPage + 1);
+      }
+    } catch (error) {
+      console.error("Помилка запиту:", error.message);
+    } finally {
+      setLoading(false);
+    }
+
+  };
+  
+useEffect(() => {
+    // 3. Перевіряємо: якщо ми ще не робили запит — робимо
+    if (!isFetched.current) {
+      fetchImages(4);
+      isFetched.current = true; // Позначаємо, що запит виконано
+    }
+  }, [useEffect]);
+
+  return (
+    <div className="PetsNews-container">
+      <p className="PetsNews-heading">
+        Interacting with our pets
+      </p>
 
 
-            const response = await axios.get(url);
+      <ul className="PetsNews-list">
 
-            if (response.data?.hits) {
-                setImages(prev => [...prev, ...response.data.hits]);
-                setPage(prevPage => prevPage + 1);
-            }
-        } catch (error) {
-            console.error("Помилка запиту:", error.message);
-        } finally {
-            setLoading(false);
-        }
+        {images.map(img => (
+         <li key={`${img.id}`}>
+            <div className="PetsNews-img-wrapper">
+                <img className="PetsNews-img" src={img.url} alt="A cute cat" />
+            </div>
+            <p className="PetsNews-list-text">Cat ID: {img.id}</p>
+          </li>
 
-    };
-    useEffect(() => {
-        fetchImages(3);
-    }, []);
+        ))}
+      </ul>
 
-    return (
-        <div className="PetsNews-container">
-            <p className="PetsNews-heading">
-                Interacting with our pets
-            </p>
-
-
-            <ul className="PetsNews-list">
-
-                {images.map(img => (
-                    <li key={img.id}>
-                        <img src={img.webformatURL} alt={img.tags} />
-                        <p className="PetsNews-list-text"> INF from API</p>
-                    </li>
-
-                ))}
-            </ul>
-
-            <button
-                className="PetsNews-btn"
-                onClick={() => fetchImages(12)}
-                disabled={loading}
-            >
-                {loading ? 'Loading...' : 'See More'}
-            </button>
-        </div>
-    );
+      <button
+        className="PetsNews-btn"
+        onClick={() => fetchImages(4)}
+        disabled={loading}
+      >
+        {loading ? 'Loading...' : 'See More'}
+      </button>
+    </div>
+  );
 };
 
 export default PetsNews;
